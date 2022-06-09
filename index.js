f1e921b046d7f03e6cae2b2d219b5f29497350a6
Amos Koech <koechronix@yahoo.com>
2022-06-09 06:38:19 -0400

    completed scope

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase ();
+
+}
+
+var bestCustomer;
+ 
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const least = 'Koech'
+
+function changeLeastFavoriteCustomer() {
+    least = 'Amos'
+    
+
+}
